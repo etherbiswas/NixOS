b2567aa34947eaf2a44a3ff0153869eed8dd63e2
Ether Biswas <etherrorcode404@gmail.com>
2023-08-23 10:22:05 +0600

    Update default.nix

--- hosts/pwnix/default.nix
+++ hosts/pwnix/default.nix
@@ -2,9 +2,9 @@
 
 {
 	imports = [
-    ../../modules/intel/default.nix # Default to intel graphics
-		# ../../modules/nvidia-optimus/default.nix # Enable this if you happen to have a nvidia optimus laptop
-    ../../modules/podman/default.nix # Enable Podman
-    ../../modules/neo4j/default.nix # Enable Neo4j
+    ../../modules/amd/default.nix # Default to amd graphics
+    # ../../modules/intel/default.nix # Default to intel graphics
+    # ../../modules/podman/default.nix # Enable Podman
+    # ../../modules/neo4j/default.nix # Enable Neo4j
 	];
 }
