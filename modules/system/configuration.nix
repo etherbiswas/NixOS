38c9dae5854ccf6f1592570469fd42c09218ed17
Ether Biswas <etherrorcode404@gmail.com>
2024-01-08 13:51:57 +0600

    Update configuration.nix

--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -34,7 +34,7 @@
   enable = true;
   settings = {
     CPU_BOOST_ON_AC = 1;
-    CPU_BOOST_ON_BAT = 0;
+    CPU_BOOST_ON_BAT = 1;
     CPU_SCALING_GOVERNOR_ON_AC = "performance";
     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
   };
