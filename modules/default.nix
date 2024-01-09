86d5120ca34b60de940e387e8771896cc3251553
Ether Biswas <etherrorcode404@gmail.com>
2024-01-09 10:24:08 +0600

    Update default.nix

--- modules/default.nix
+++ modules/default.nix
@@ -4,14 +4,10 @@
   home.stateVersion = "23.11";
   imports = [
     ./packages
-    ./hyprland
-    ./waybar
     ./alacritty
     ./fish
     ./gtk
-    ./direnv
     ./dunst
-    ./ulauncher
     # ./pwnixos-packages
   ];
 }
